a1b702cfc045c01d7e189a4270dce7feb9751721
Jan Nash <jnash@jnash.de>
2017-11-09 00:30:49 +0100

    Start adding docstring to DataSource

--- Decoupler/Code/DataSource.swift
+++ Decoupler/Code/DataSource.swift
@@ -9,6 +9,34 @@
 
 // MARK: // Public
 // MARK: Protocol Declaration
+/*
+ A DataSource is supposed to:
+ - provide data-records on request
+ 
+ It could also (but does not have to):
+ - save changed records
+ - delete records
+ 
+ It should not:
+ - map the data between different representations
+ - change the data
+ - cache the data (since a cache is a datasource itself)
+ 
+ Records can be requested in two different ways:
+ - by ID
+ - by filtering
+ 
+ A client passes either:
+ - a single ID
+ - a set of IDs
+ - one or more filters
+ and receives a Result ???
+ 
+ This can happen either synchronously or asynchronously.
+ (though that's out of the scope of this abstract protocol)
+ 
+ A DataSource can have multiple clients.
+ */
 protocol DataSource {
     var clients: DataSourceClient { get }
 }
